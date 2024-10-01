edb921266f43a4e9c523b453c2488d5a737a7396
stephen447 <byrnes67@tcd.ie>
2024-10-01 18:04:03 +0100

    adding theme persistency on refresh

--- messaging-app-frontend/src/components/ThemeToggle/ThemeToggle.jsx
+++ messaging-app-frontend/src/components/ThemeToggle/ThemeToggle.jsx
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from "react";
 import "../../App.css";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState("light");
+  // Get the theme from the local storage
+  let localTheme = localStorage.getItem("theme");
+  if (!localTheme) {
+    // Set the theme to light if no theme is found in local storage
+    localStorage.setItem("theme", "light");
+    localTheme = "light";
+  }
+
+  const [theme, setTheme] = useState(localTheme);
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
